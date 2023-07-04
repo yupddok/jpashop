7fa0902cee1692614e77e6084997ecf5971e4e7a
yupddok <yupddok@gmail.com>
2023-07-04 23:19:57 +0900

    엔티티 클래스 개발1 - 외래 키가 있는 곳을 연관관계의 주인으로 정해라 - Enum 타입 사용시 -- @Enumerated 애노테이션 필수 -- EnumType.ORDINAL(컬럼이 1,2,3 숫자로 들어감 but Enum추가 되면 꼬임) -> 절대 사용금지

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    //Enum 타입 사용시
+    //@Enumerated 애노테이션 필수
+    //EnumType.ORDINAL(컬럼이 1,2,3 숫자로 들어감 but Enum추가 되면 꼬임) -> 절대 사용금지
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; //READY, COMP
+}
